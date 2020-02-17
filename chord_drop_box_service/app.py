e0529e78c1e6b5a5e3713b6df318c267deb31aac
Simon Chénard <chenard.simon@gmail.com>
2020-02-17 15:54:52 -0500

    connect to minIO if the var env are provided - will need a refactor, still WIP

--- chord_drop_box_service/app.py
+++ chord_drop_box_service/app.py
@@ -1,29 +1,39 @@
 import os
-
-import chord_drop_box_service
 import sys
 import traceback
 
-from chord_lib.auth.flask_decorators import flask_permissions_owner
-from chord_lib.responses.flask_errors import *
+import boto3
 from flask import Flask, jsonify, request, send_file
 from werkzeug.exceptions import BadRequest, NotFound
 from werkzeug.utils import secure_filename
+
+import chord_drop_box_service
+from chord_lib.auth.flask_decorators import flask_permissions_owner
+from chord_lib.responses.flask_errors import *
 
 
 SERVICE_TYPE = "ca.c3g.chord:drop-box:{}".format(chord_drop_box_service.__version__)
 SERVICE_ID = os.environ.get("SERVICE_ID", SERVICE_TYPE)
 
+SERVICE_DATA = os.environ.get("SERVICE_DATA", "data/")
+MINIO_URL = os.environ.get("MINIO_URL", None)
+
 application = Flask(__name__)
 application.config.from_mapping(
     SERVICE_TYPE=SERVICE_TYPE,
     SERVICE_ID=SERVICE_ID,
-    SERVICE_DATA=os.environ.get("SERVICE_DATA", "data/")
+    SERVICE_DATA_SOURCE='minio' if MINIO_URL else 'local',
+    MINIO_URL=MINIO_URL,
+    MINIO_USERNAME=os.environ.get('MINIO_USERNAME') if MINIO_URL else None,
+    MINIO_PASSWORD=os.environ.get('MINIO_PASSWORD') if MINIO_URL else None,
+    MINIO_BUCKET=os.environ.get('MINIO_BUCKET') if MINIO_URL else None,
+    SERVICE_DATA=None if MINIO_URL else SERVICE_DATA
 )
 
 
 # Make data directory/ies if needed
-os.makedirs(application.config["SERVICE_DATA"], exist_ok=True)
+if application.config['SERVICE_DATA_SOURCE'] == 'local':
+    os.makedirs(application.config["SERVICE_DATA"], exist_ok=True)
 
 
 TRAVERSAL_LIMIT = 16
@@ -49,6 +59,16 @@ application.register_error_handler(BadRequest, _wrap(flask_bad_request_error))
 application.register_error_handler(NotFound, _wrap(flask_not_found_error))
 
 
+if application.config['SERVICE_DATA_SOURCE'] == 'minio':
+    minio = boto3.resource(
+        's3',
+        endpoint_url=application.config['MINIO_URL'],
+        aws_access_key_id=application.config['MINIO_USERNAME'],
+        aws_secret_access_key=application.config['MINIO_PASSWORD']
+    )
+    bucket = minio.Bucket(application.config['MINIO_BUCKET'])
+
+
 def recursively_build_directory_tree(directory, level=0):
     return tuple({"name": entry,
                   "path": os.path.abspath(os.path.join(directory, entry)),
@@ -61,15 +81,144 @@ def recursively_build_directory_tree(directory, level=0):
                  if (level < TRAVERSAL_LIMIT or not os.path.isdir(os.path.join(directory, entry))) and entry[0] != ".")
 
 
+class S3File():
+    def __init__(self, obj, name):
+        self.name = name
+        self.path = obj.key
+        self.size = obj.size
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "path": self.path,
+            "size": self.size
+        }
+
+
+class S3Directory():
+    # The path argument keeps track of where we are in the nested
+    # directories
+    def __init__(self, obj, path=None):
+        self.directories = []
+        self.files = []
+
+        if '/' not in obj.key:
+            raise Exception('Not a directory')
+
+        if path:
+            path_parts = path.split('/')
+        else:
+            path_parts = obj.key.split('/')
+
+        self.name = path_parts[0]
+
+        if len(path_parts) > 1:
+            remaining_path = '/'.join(path_parts[1:])
+            self.add_path(obj, remaining_path)
+        else:
+            file_obj = S3File(obj, path_parts[0])
+            self.files.append(file_obj)
+
+    def dir_exists(self, name):
+        for d in self.directories:
+            if d.name == name:
+                return d
+        
+        return None
+
+    def add_path(self, obj, path):
+        if '/' in path:
+            path_parts = path.split('/')
+            directory = self.dir_exists(path_parts[0])
+
+            if directory:
+                directory.add_path(obj, '/'.join(path_parts[1:]))
+            else:
+                directory = S3Directory(obj, path)
+                self.directories.append(directory)
+        else:
+            file_obj = S3File(obj, path)
+            self.files.append(file_obj)
+
+    def serialize(self):
+        return {
+            "contents": [entry.serialize() for entry in self.files + self.directories],
+            "name": self.name
+        }
+
+
+class S3Tree():
+    def __init__(self):
+        self.directories = []
+        self.files = []
+
+    def dir_exists(self, name):
+        for d in self.directories:
+            if d.name == name:
+                return d
+        
+        return None
+
+    def add_path(self, obj):
+        if '/' in obj.key:
+            path_parts = obj.key.split('/')
+            directory = self.dir_exists(path_parts[0])
+
+            if directory:
+                directory.add_path(obj, '/'.join(path_parts[1:]))
+            else:
+                directory = S3Directory(obj)
+                self.directories.append(directory)
+        else:
+            file_obj = S3File(obj, obj.key)
+            self.files.append(file_obj)
+
+    def serialize(self):
+        return tuple(
+            entry.serialize() for entry in self.files + self.directories
+        )
+
+
+def minio_build_directory_tree():
+    tree = S3Tree()
+
+    for obj in bucket.objects.all():
+        tree.add_path(obj)
+
+    return tree.serialize()
+
+
+#def minio_build_directory_tree(bucket):
+#    bucket = minio.Bucket('patate')
+#    objs = bucket.objects.all()
+#    dir_tree = []
+#
+#    dirs = []
+#    files = []
+#
+#    for obj in objs:
+#        if '/' in obj.key:
+#            for part in reversed(obj.key.split('/')):
+#            entry = {
+#                "name": obj.key.split('/')[0],
+#                "path": obj.key.split('/')[0],
+#                "contents": minio_parse_directory_tree(obj.key)
+#        else:
+#            files.append(obj)
+
+
 @application.route("/tree", methods=["GET"])
 @flask_permissions_owner
 def drop_box_tree():
-    return jsonify(recursively_build_directory_tree(application.config["SERVICE_DATA"]))
+    if application.config['SERVICE_DATA_SOURCE'] == 'local':
+        return jsonify(recursively_build_directory_tree(application.config["SERVICE_DATA"]))
+    elif application.config['SERVICE_DATA_SOURCE'] == 'minio':
+        return jsonify(minio_build_directory_tree())
+    else:
+        raise flask_internal_server_error("The service source data is not configured properly")
 
 
-@application.route("/objects/<path:path>", methods=["GET", "PUT"])
-@flask_permissions_owner
-def drop_box_retrieve(path):
+def locally_retrieve(path):
     directory_items = recursively_build_directory_tree(application.config["SERVICE_DATA"])
 
     if request.method == "PUT":
@@ -126,6 +275,21 @@ def drop_box_retrieve(path):
 
         except StopIteration:
             return flask_not_found_error("Nothing found at specified path")
+
+
+def minio_retrieve(path):
+    return flask_not_found_error("Nothing found at specified path")
+
+
+@application.route("/objects/<path:path>", methods=["GET", "PUT"])
+@flask_permissions_owner
+def drop_box_retrieve(path):
+    if application.config['SERVICE_DATA_SOURCE'] == 'local':
+        return locally_retrieve(path)
+    elif application.config['SERVICE_DATA_SOURCE'] == 'minio':
+        return minio_retrieve(path)
+    else:
+        raise flask_internal_server_error("The service source data is not configured properly")
 
 
 @application.route("/service-info", methods=["GET"])
