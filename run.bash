bcce0611925b8781f27739ada2adcea2cc970cdf
David Lougheed <david.lougheed@gmail.com>
2023-06-05 13:33:58 -0400

    chore: clean up prod runner script

--- run.bash
+++ run.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ -z "${INTERNAL_PORT}" ]; then
-  # Set default internal port to 5000
-  export INTERNAL_PORT=5000
-fi
+export ASGI_APP='bento_drop_box_service.app:application'
 
-uvicorn bento_drop_box_service.app:application \
+# Set default internal port to 5000
+: "${INTERNAL_PORT:=5000}"
+
+uvicorn "${ASGI_APP}" \
   --workers 1 \
   --loop uvloop \
   --host 0.0.0.0 \
