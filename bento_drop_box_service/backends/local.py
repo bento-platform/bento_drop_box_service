adc6fc3f001cb02697eab7e952a577c014bdc9ae
Victor Rocheleau <victor.rocheleau@mcgill.ca>
2025-04-14 16:26:52 -0400

    make tree path check absolute

--- bento_drop_box_service/backends/local.py
+++ bento_drop_box_service/backends/local.py
@@ -68,7 +68,7 @@ class LocalBackend(DropBoxBackend):
         if sub_path:
             root_path = root_path.joinpath(sub_path)
 
-        if not str(root_path).startswith(self.config.service_data):
+        if not str(root_path.absolute()).startswith(self.config.service_data):
             # Only accept requests that are under the data volume
             self.logger.warning(f"attempted to get directory tree outside of drop box data volume: {root_path}")
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Cannot inspect provided sub tree")
