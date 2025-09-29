3889a78d70799db61f3c670c161a0c29f365b03d
Gordon Krieger <gordon.krieger@gmail.com>
2025-09-29 14:16:36 -0400

    fix tests for tree filtering

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,37 +1,48 @@
 from fastapi import Response
 from fastapi.testclient import TestClient
+from bento_drop_box_service.backends.base import DropBoxEntry
 
 
-def validate_tree(res: Response):
-    tree = res.json()
+def validate_tree(tree: list[DropBoxEntry]):
 
-    assert res.status_code == 200
     assert len(tree) == 4
-    assert tree[0]["name"] == "patate.txt"
-    assert "contents" in tree[1]
+    root_expected_names = ["tomate.vcf", "zucchini.json", "patate.txt", "some_dir"]
+    some_dir_expected_names = ["tomate.vcf", "zucchini.json", "patate.txt", "some_other_dir", "empty_dir"]
+    some_other_dir_expected_names = ["tomate.vcf", "zucchini.json", "patate.txt"]
+
+    root_names = [entry["name"] for entry in tree]
+    assert sorted(root_names) == sorted(root_expected_names)
+
+    some_dir = next((entry for entry in tree if entry["name"] == "some_dir"), None)
+    some_dir_names = [entry["name"] for entry in some_dir["contents"]]
+    assert sorted(some_dir_names) == sorted(some_dir_expected_names)
+
+    some_other_dir = next((entry for entry in some_dir["contents"] if entry["name"] == "some_other_dir"), None)
+    some_other_dir_names = [entry["name"] for entry in some_other_dir["contents"]]
+    assert sorted(some_other_dir_names) == sorted(some_other_dir_expected_names)
 
 
-def validate_subtree(res: Response):
-    tree = res.json()
-
-    assert res.status_code == 200
+def validate_subtree_some_dir(tree: list[DropBoxEntry]):
     assert len(tree) == 5
-    assert tree[1]["name"] == "patate.txt"
-    assert "contents" in tree[0]
-    assert "contents" in tree[2]
+    some_dir_expected_names = ["tomate.vcf", "zucchini.json", "patate.txt", "some_other_dir", "empty_dir"]
+    some_dir_names = [entry["name"] for entry in tree]
+    assert sorted(some_dir_names) == sorted(some_dir_expected_names)
 
 
-def validate_filtered_tree(res: Response, expected_files: list[str], num_directories: int = 1):
-    tree = res.json()
-
-    assert res.status_code == 200
-    assert len(tree) == num_directories + len(expected_files)
-
-    for index in range(len(expected_files)):
-        assert tree[index + num_directories]["name"] == expected_files[index]
-
-    for dir in tree[0]["contents"]:
-        assert dir["name"] in expected_files or "empty_dir"
+def validate_filtered_tree(tree: list[DropBoxEntry], expected_files: list[str], expected_directories: list[str]):
+    if not expected_files:
+        assert len(tree) == 0
+    else:
+        for node in tree:
+            if node.get("uri"):
+                # entry for a file
+                assert node["name"] in expected_files
+            else:
+                # else directory
+                assert node["name"] in expected_directories
+                assert node["name"] != "empty_dir"
+                if child_contents := node.get("contents"):
+                    validate_filtered_tree(child_contents, expected_files, expected_directories)
 
 
 def test_service_info(client_local: TestClient):
@@ -45,19 +56,20 @@ def test_service_info(client_local: TestClient):
 
 def test_tree_local(client_local: TestClient):
     res = client_local.get("/tree")
-    validate_tree(res)
+    assert res.status_code == 200
+    validate_tree(res.json())
 
     res = client_local.get("/tree?include=json")
-    validate_filtered_tree(res, ["zucchini.json"])
+    validate_filtered_tree(res.json(), ["zucchini.json"], ["some_dir", "some_other_dir"])
 
     res = client_local.get("/tree?include=json&include=.vcf")
-    validate_filtered_tree(res, ["tomate.vcf", "zucchini.json"])
+    validate_filtered_tree(res.json(), ["tomate.vcf", "zucchini.json"], ["some_dir", "some_other_dir"])
 
     res = client_local.get("/tree?ignore=txt")
-    validate_filtered_tree(res, ["tomate.vcf", "zucchini.json"])
+    validate_filtered_tree(res.json(), ["tomate.vcf", "zucchini.json"], ["some_dir", "some_other_dir"])
 
     res = client_local.get("/tree?ignore=txt&ignore=.json&ignore=.vcf")
-    validate_filtered_tree(res, [])
+    validate_filtered_tree(res.json(), [], [])
 
     res = client_local.get("/tree?include=txt&ignore=json")
     assert res.status_code == 400
@@ -65,13 +77,14 @@ def test_tree_local(client_local: TestClient):
 
 def test_tree_subpath_local(client_local: TestClient):
     res = client_local.get("/tree/some_dir")
-    validate_subtree(res)
+    assert res.status_code == 200
+    validate_subtree_some_dir(res.json())
 
     res = client_local.get("/tree/some_dir?include=json")
-    validate_filtered_tree(res, ["zucchini.json"], 2)
+    validate_filtered_tree(res.json(), ["zucchini.json"], ["some_other_dir"])
 
     res = client_local.get("/tree/some_dir?ignore=txt")
-    validate_filtered_tree(res, ["tomate.vcf", "zucchini.json"], 2)
+    validate_filtered_tree(res.json(), ["tomate.vcf", "zucchini.json"], ["some_other_dir"])
 
     res = client_local.get("/tree/some_dir?include=txt&ignore=json")
     assert res.status_code == 400
